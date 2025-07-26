a76fe44af0705210d590ff601063411f379af8d4
deyaaomran <156389291+deyaaomran@users.noreply.github.com>
2025-07-26 15:40:52 +0300

    APIs

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
-    return HttpResponse( 'Hello World!')
+    pass
+def about(request):
+    pass
